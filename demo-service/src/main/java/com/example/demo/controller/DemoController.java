a6b9f007669c39cb7f13b3c0cee3263eb936dc10
高振龙 <gzl1988@163.com>
2017-11-08 15:40:31 +0800

    test proj

--- demo-service/src/main/java/com/example/demo/controller/DemoController.java
+++ demo-service/src/main/java/com/example/demo/controller/DemoController.java
@@ -1,5 +1,8 @@
 package com.example.demo.controller;
 
+import com.example.demo.ServiceB;
+import com.example.demo.ServiceC;
+import org.springframework.beans.factory.serviceloader.ServiceFactoryBean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +12,14 @@ public class DemoController {
     @RequestMapping("/hello")
     String home() {
         return "Hello World!\n";
+    }
+
+    @RequestMapping("/depTest")
+    String depTest() {
+        ServiceB serviceB = new ServiceB();
+        serviceB.method1();
+        ServiceC serviceC = new ServiceC();
+        serviceC.method1();
+        return "ok";
     }
 }
